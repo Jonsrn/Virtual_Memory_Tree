5578a17c4fcbb73b6c4f6ec1ca66efb2b00d7faa
Jonsrn <jonathansrn2@gmail.com>
2024-11-08 02:07:17 -0300

    Add files via upload

---
+++ Memoria_Virtual/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "Gerenciamento_Memoria.h"
+
+int main(){
+
+    Arv45Mem *Raiz; 
+    Raiz = NULL; 
+    int op1, op2; 
+
+    do{
+        menu_principal(); 
+        scanf("%d", &op1); 
+
+        switch(op1){
+
+            case 0: 
+               break; 
+            case 1: 
+               //Monta os blocos
+               break; 
+            case 2: 
+                //Funcionalidades
+                do{
+                    funcionalidades(); 
+                    scanf("%d", &op2);
+
+                    switch(op2){
+                        case 0: 
+                           break;
+                        case 1: 
+                             //Alocar Memória 
+                             break; 
+                        case 2: 
+                             //Liberar Memória
+                             break; 
+                        default: 
+                           printf("Opção Inválida, tente novamente\n");  
+                    }
+
+                }while(op2 != 0); 
+
+
+                break; 
+            case 3: 
+                sobre(); 
+                break; 
+            default: 
+              printf("Opção inválida, digite novamente\n"); 
+        }  
+    
+    
+    
+    }while(op1 != 0); 
+
+    return 0; 
+}
