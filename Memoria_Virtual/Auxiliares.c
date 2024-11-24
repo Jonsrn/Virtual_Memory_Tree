83636cc312dd009a418f48cde9070efb97904f82
Jonsrn <jonathansrn2@gmail.com>
2024-11-24 02:39:35 -0300

    Update Auxiliares.c

--- Memoria_Virtual/Auxiliares.c
+++ Memoria_Virtual/Auxiliares.c
@@ -27,6 +27,8 @@ void construir_memoria_do_sistema(Arv45Mem **Raiz) {
             }
             scanf("%d", &Informacao.intervalo);
 
+            //falta uma validação aqui, se digitar 0 ou negativo
+
             // Ajustar o bloco final baseado no intervalo
             Informacao.bloco_fim = Informacao.bloco_inicio + Informacao.intervalo - 1;
 
@@ -72,19 +74,16 @@ void alocar_memoria_para_programa(Arv45Mem **Raiz){
         printf("Digite quantos blocos de memória (1mb) que o programa precisa: ");
         scanf("%d", &qtd_blocos); 
         Inf45 *bloco_anterior;
-
-        bloco_anterior = NULL; 
-        
-
+        bloco_anterior = NULL;
         situacao = 1; //deu certo
 
 
 
        operacao = alocar_memoria((*Raiz), qtd_blocos); 
        if(operacao == 2 || situacao == 3 || situacao == 4){
-           ajustando_os_intervalos(*Raiz, &bloco_anterior, 1); 
+           ajustando_os_intervalos(*Raiz, &bloco_anterior, 1); // A alocação não usou o bloco final
        }else if(operacao == 5){
-           ajustando_os_intervalos(*Raiz, &bloco_anterior, 2); 
+           ajustando_os_intervalos(*Raiz, &bloco_anterior, 2); //A alocação usou o bloco final
            
        }
        
@@ -101,6 +100,7 @@ void alocar_memoria_para_programa(Arv45Mem **Raiz){
           //A quantidade de memoria foi reservada, mas como não há blocos ocupados, um precisa ser criado 
           // chama a função de criação do Nó, coleta o Último bloco da Info1, e adiciona 1 pra ser o inicio, e depois adiciona o intervalo
           
+
           if(operacao != 1){
               //A operação falhou, pois houve falha na criação de um novo bloco
               situacao = 3;
@@ -132,4 +132,78 @@ void alocar_memoria_para_programa(Arv45Mem **Raiz){
 }
 
 
+void desalocar_memoria_sistema(Arv45Mem **Raiz){
+   int situacao; 
 
+   if(*Raiz != NULL){
+       int qtd_blocos, operacao; 
+       printf("Essa é a liberação de memória do sistema\n"); 
+       printf("Digite quantos blocos de memória (1mb) que deseja liberar: ");
+       scanf("%d", &qtd_blocos); 
+       Inf45 *bloco_anterior;  
+       bloco_anterior = NULL; 
+       situacao = 1; 
+
+       operacao = desalocar_memoria((*Raiz), qtd_blocos); 
+       if(operacao == 2 || situacao == 3 || situacao == 4){
+           ajustando_os_intervalos(*Raiz, &bloco_anterior, 1); // A alocação não usou o bloco final
+       }else if(operacao == 5){ 
+           ajustando_os_intervalos(*Raiz, &bloco_anterior, 2); //A alocação usou o bloco final
+           
+       }
+
+       if(operacao == 0){
+            //Não há memória suficiente pra ser liberada, não faço mais nada
+            situacao = 4;
+       }else if(operacao == 1){
+          //A quantidade de memoria foi reservada, mas como não há blocos livres, um precisa ser criado 
+          // chama a função de criação do Nó, coleta o Último bloco da Info1, e adiciona 1 pra ser o inicio, e depois adiciona o intervalo
+          
+
+          if(operacao != 1){
+              //A operação falhou, pois houve falha na criação de um novo bloco
+              situacao = 3;
+          }
+
+          //o retorno com sucesso
+       }else if(operacao == 3){
+          //3 significa que tudo deu certo, mas um bloco precisa ser apagado
+
+          
+
+          
+
+          //aqui chama a função que vai excluir o Nó. 
+
+          if(operacao != 1){
+               //houve falha na exclusão do Nó
+               situacao = 2;
+          }
+       }
+
+
+
+
+
+
+      
+       
+
+
+
+
+
+
+
+
+   }else{
+      //Não há dataset montado
+      situacao = 0; 
+   }
+
+
+   situacao_desalocacao_memoria(situacao); 
+
+
+
+}
