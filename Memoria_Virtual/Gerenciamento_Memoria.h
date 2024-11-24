f0ea0cb41207304a50c2aa64f24a19d393597cbd
Jonsrn <jonathansrn2@gmail.com>
2024-11-23 23:49:10 -0300

    Update Gerenciamento_Memoria.h

--- Memoria_Virtual/Gerenciamento_Memoria.h
+++ Memoria_Virtual/Gerenciamento_Memoria.h
@@ -3,6 +3,8 @@
 
 #define LIVRE 0
 #define OCUPADO 1
+#define APAGAR 2
+#define MANTER 3
 
 //Protótipo das Estruturas
 
@@ -10,7 +12,7 @@ typedef struct Info45MEM Inf45; //prototipo da informação presente em cada Inf
 typedef struct Arvore45_Memoria Arv45Mem; //Prototipo da Estrutura da árvore 4-5
 
 typedef struct Info45MEM{
-    int bloco_inicio, bloco_fim, intervalo, status;
+    int bloco_inicio, bloco_fim, intervalo, status, status_apagar;
 }Inf45;
 
 typedef struct Arvore45_Memoria{
@@ -24,6 +26,15 @@ typedef struct Arvore45_Memoria{
 void menu_principal(); //Menu principal
 void sobre();//O sobre
 void funcionalidades(); //O submenu das Funcionalidades. 
+
+//Protótipo das funções de exibição de status
+
+void situacao_construcao_memoria(int situacao); 
+void situacao_alocacao_memoria(int situacao);
+
+
+
+
 
 //Protótipos das Funções da Árvore 4-5
 
@@ -39,7 +50,12 @@ void imprimirArvore45(Arv45Mem *raiz); //Função de impressão
 
 //Protótipo das Funções Intermediarias
 
-void construir_memoria_do_sistema(Arv45Mem **Raiz); 
+void construir_memoria_do_sistema(Arv45Mem **Raiz); //Função que monta o dataset
+void alocar_memoria_para_programa(Arv45Mem **Raiz); //Função auxiliar que cuida de alocar memória para uma "aplicação", se tiver espaço disponivel
+int alocar_memoria(Arv45Mem *Raiz, int qtd_blocos); //Função de alocação de memória
+Arv45Mem *atualizar_bloco(Arv45Mem *Raiz, int qtd_blocos, int operacao, int localizacao_info, int *situacao); //Função que atualiza os blocos, seja inserção ou remoção;
+int ajustando_os_intervalos(Arv45Mem *Raiz, Inf45 **bloco_anterior, int opcao); 
+
 
 
 
