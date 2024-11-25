a9fc668fdbf852103a73081b71dff2419ed4817f
Jonsrn <jonathansrn2@gmail.com>
2024-11-25 17:06:00 -0300

    Update Gerenciamento_Memoria.h

--- Memoria_Virtual/Gerenciamento_Memoria.h
+++ Memoria_Virtual/Gerenciamento_Memoria.h
@@ -12,7 +12,7 @@ typedef struct Info45MEM Inf45; //prototipo da informação presente em cada Inf
 typedef struct Arvore45_Memoria Arv45Mem; //Prototipo da Estrutura da árvore 4-5
 
 typedef struct Info45MEM{
-    int bloco_inicio, bloco_fim, intervalo, status, status_apagar;
+    int bloco_inicio, bloco_fim, intervalo, status, status_apagar, tam_total;
 }Inf45;
 
 typedef struct Arvore45_Memoria{
@@ -48,7 +48,7 @@ void imprimirArvore45(Arv45Mem *raiz); //Função base de impressão
 void imprimirInfo(Inf45 info); //imprime informações detalhadas de cada Info
 int percorrer_recuperar_Infos(Arv45Mem *Raiz, Inf45 ***vetor_recuperado, int *numero_infos); //Essa função percorre a árvore original, e faz uma cópia, excluindo as INFOS com a TAG APAGAR
 void liberarArvore45(Arv45Mem **Raiz); //Libera a arvore 4-5
-
+int eh_ultimo(Arv45Mem *Raiz, int localizacao_info); //Função pra saber se estou no ultimo bloco
 
 
 
@@ -67,7 +67,7 @@ int alocar_memoria(Arv45Mem *Raiz, int qtd_blocos); //Função de alocação de 
 int desalocar_memoria(Arv45Mem *Raiz, int qtd_blocos); //Função de desalocação de memória
 Arv45Mem *atualizar_bloco(Arv45Mem *Raiz, int qtd_blocos, int operacao, int localizacao_info, int *situacao); //Função que atualiza os blocos, seja inserção ou remoção;
 int ajustando_os_intervalos(Arv45Mem *Raiz, Inf45 **bloco_anterior, int opcao); //função que passa um "pente-fino" garantindo que não tenha espaços em branco entre os intervalos após as operações. 
-int agrupar_infos(Arv45Mem *Raiz, Inf45 **info_anterior); 
+int agrupar_infos(Arv45Mem *Raiz, Inf45 **info_anterior); //Passa verificando se há blocos adjacentes iguais (desconsiderando os que estão na pendencia de serem apagados)
 
 
 
